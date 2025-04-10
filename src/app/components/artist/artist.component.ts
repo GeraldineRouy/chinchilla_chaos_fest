eacc735f8a811e3f6a0440989c8e43a0b5c4c5e8
geraldine.rouy <geraldine.rouy@gmail.com>
2025-04-10 18:40:22 +0200

    added some effects/animation when an artist is deleted

--- src/app/components/artist/artist.component.ts
+++ src/app/components/artist/artist.component.ts
@@ -1,23 +1,26 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {Artist} from './artist';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Artist } from './artist';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'artist',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './artist.component.html',
   styleUrl: './artist.component.css'
 })
 export class ArtistComponent {
-
   @Input() artist!: Artist;
+  @Output() artistDeleted = new EventEmitter<void>();
 
-  @Output() artistDeleted = new EventEmitter();
+  isDeleting = false;
 
   clickButtonToDelete(): void {
     const confirmed = confirm(`Are you sure you want to delete "${this.artist.name}"?`);
     if (confirmed) {
-      this.artistDeleted.emit();
+      this.isDeleting = true;
+      setTimeout(() => {
+        this.artistDeleted.emit();
+      }, 1000);
     }
   }
-
 }
